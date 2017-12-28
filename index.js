51ef9626faf3b0f1464c407d2bc76093788a78c4
Robert Kirby <robertaxelkirby@gmail.com>
2017-12-28 11:06:45 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$(".js-show-more").on('click', function(event) {
+	// alter 'show more/less' text
+	if ($(event.target).html() == 'Show rest of testimonial') {
+		$(event.target).html('Show less');
+	} else {
+		$(event.target).html('Show rest of testimonial');
+	}
+
+	// show/hide review
+	let text = $(event.target).prev('.js-testimonial-text')
+	$(text).toggleClass('testimonial-text-hide')
+});
